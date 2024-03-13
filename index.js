ca1a5302d74a6a38a44c0f597fc85f0d24123d90
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-03-13 22:18:04 +0530

    delete

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express=require("express")
-const app=express()
 const cors=require("cors")
+const app=express()
+
 require("./mongoosconn01")
-const studentmodel=require("./studentschema02")
 app.use(express.json())
 app.use(cors())
+const studentmodel=require("./studentschema02")
+
 app.get("/",async (req,resp)=>
 {
 const result=await studentmodel.find()
@@ -14,9 +16,19 @@ app.post("/",async(req,resp)=>{
     const data=await new studentmodel(req.body)
     const result=await data.save()
     resp.send(result)
-}
-)
+})
+
+
+app.delete("/",async (req,resp)=>
+{
+const result=await studentmodel.deleteOne({rollno:req.body.rollno})
+resp.send(result)
+})
 app.listen(4000)
+
+
+
+
 
 
 
