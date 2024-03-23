7c5f90a41c059d41f3ff91e1e17b2c2176e6818d
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-03-23 15:46:57 +0530

    new

--- index.js
+++ index.js
@@ -30,41 +30,40 @@ app.listen(4000)
 
 // file upload
 
-// const express=require("express")
-// const app=express()
-// const multer=require("multer")
-// const path=require("path")
-// const fs=require("fs")
-// require("./mongoosconn01")
-// const ImageModel=require("./studentschema02")
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null,"public/uploads" );
-//     },
-//     filename: (req, file, cb) => {
-//       cb(null,file.originalname);
-//     }
-//   });
-//   const upload = multer({ storage: storage }).single("file");
-//  app.post("/",(req,resp)=>
-//  {
-//    // resp.send("upload")
-//     upload(req,resp,(err)=>{
-//         if(err)
-//         {
-//           console.log(err)
-//         }
-//         else{
-//             const newImage=new ImageModel({
-//                 name:req.body.name,
-//                 image:"http://localhost:4000/uploads"+req.file.filename
-//             })
-//              newImage.save()
-//              resp.send("File Uploaded") 
-//         }
-//     })
-//  })
-//  app.listen(4000)
+
+const multer=require("multer")
+const path=require("path")
+const fs=require("fs")
+require("./mongoosconn01")
+const ImageModel=require("./studentschema02")
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null,"public/uploads" );
+    },
+    filename: (req, file, cb) => {
+      cb(null,file.originalname);
+    }
+  });
+  const upload = multer({ storage: storage }).single("file");
+ app.post("/",(req,resp)=>
+ {
+   // resp.send("upload")
+    upload(req,resp,(err)=>{
+        if(err)
+        {
+          console.log(err)
+        }
+        else{
+            const newImage=new ImageModel({
+                name:req.body.name,
+                image:"https://hclapi-9wny.onrender.com/"+req.file.filename
+            })
+             newImage.save()
+             resp.send("File Uploaded") 
+        }
+    })
+ })
+ app.listen(4000)
 
 
 
