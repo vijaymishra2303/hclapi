aff8a3f88745aea01147d715b0779e897cc2c6e4
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-03-23 15:56:10 +0530

    new

--- ex.js
+++ ex.js
@@ -1,31 +1,31 @@
-const express=require("express")
-const cors=require("cors")
-const app=express()
+// const express=require("express")
+// const cors=require("cors")
+// const app=express()
 
-require("./mongoosconn01")
-app.use(express.json())
-app.use(cors())
-const studentmodel=require("./studentschema02")
+// require("./mongoosconn01")
+// app.use(express.json())
+// app.use(cors())
+// const studentmodel=require("./studentschema02")
 
-app.get("/",async (req,resp)=>
-{
-const result=await studentmodel.find()
-resp.send(result)
-})
+// app.get("/",async (req,resp)=>
+// {
+// const result=await studentmodel.find()
+// resp.send(result)
+// })
 
-app.post("/",async(req,resp)=>{
-    const data=await new studentmodel(req.body)
-    const result=await data.save()
-    resp.send(result)
-})
+// app.post("/",async(req,resp)=>{
+//     const data=await new studentmodel(req.body)
+//     const result=await data.save()
+//     resp.send(result)
+// })
 
 
-app.delete("/",async (req,resp)=>
-{
-const result=await studentmodel.deleteOne({rollno:req.body.rollno})
-resp.send(result)
-})
-app.listen(4000)
+// app.delete("/",async (req,resp)=>
+// {
+// const result=await studentmodel.deleteOne({rollno:req.body.rollno})
+// resp.send(result)
+// })
+// app.listen(4000)
 
 
 
