ed74f67dcf7bcfac488f4c38a406550ebedba957
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-03-23 18:21:52 +0530

    cc

--- index.js
+++ index.js
@@ -26,44 +26,51 @@ const result=await studentmodel.deleteOne({rollno:req.body.rollno})
 resp.send(result)
 })
 
+app.put("/",async (req,resp)=>
+{
+const result=await studentmodel.updateOne({rollno:req.body.rollno})
+resp.send(result)
+})
+app.listen(4000)
 
 
 // file upload
 
-
-const multer=require("multer")
-const path=require("path")
-const fs=require("fs")
-require("./mongoosconn01")
-const ImageModel=require("./studentschema02")
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null,"hcl/students" );
-    },
-    filename: (req, file, cb) => {
-      cb(null,file.originalname);
-    }
-  });
-  const upload = multer({ storage: storage }).single("file");
- app.post("/",(req,resp)=>
- {
-   // resp.send("upload")
-    upload(req,resp,(err)=>{
-        if(err)
-        {
-          console.log(err)
-        }
-        else{
-            const newImage=new ImageModel({
-                name:req.body.name,
-                image:"https://hclapi-9wny.onrender.com/"+req.file.filename
-            })
-             newImage.save()
-             resp.send("File Uploaded") 
-        }
-    })
- })
- app.listen(4000)
+// const express=require("express")
+// const app=express()
+// const multer=require("multer")
+// const path=require("path")
+// const fs=require("fs")
+// require("./mongoosconn01")
+// const ImageModel=require("./studentschema02")
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null,"public/uploads" );
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null,file.originalname);
+//     }
+//   });
+//   const upload = multer({ storage: storage }).single("file");
+//  app.post("/",(req,resp)=>
+//  {
+//    // resp.send("upload")
+//     upload(req,resp,(err)=>{
+//         if(err)
+//         {
+//           console.log(err)
+//         }
+//         else{
+//             const newImage=new ImageModel({
+//                 name:req.body.name,
+//                 image:"http://localhost:4000/uploads"+req.file.filename
+//             })
+//              newImage.save()
+//              resp.send("File Uploaded") 
+//         }
+//     })
+//  })
+//  app.listen(4000)
 
 
 
